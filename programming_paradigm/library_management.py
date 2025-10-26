6b740f19f0ea2ec70082f86b75e0c9d92d24f7bd
Ebubechukwu Victory <105538589+VictoryNruama@users.noreply.github.com>
2025-10-26 23:26:25 +0100

    Update library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,31 @@
+# library_management.py
+
+class Book:
+    """A class representing a book in the library."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute to track availability
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False  # already checked out
+
+    def return_book(self):
+        """Mark the book as returned."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False  # book was not checked out
+
+    def is_available(self):
+        """Return True if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    """A cla
